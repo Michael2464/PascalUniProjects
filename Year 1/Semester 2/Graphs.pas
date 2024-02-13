a5a23caac4490b915deba6210d2f843bff7f3fb6
unknown <mzyranov26@gmail.com>
2024-02-13 16:25:48 +0400

    Graphs.ps update 2

--- Year 1/Semester 2/Graphs.pas
+++ Year 1/Semester 2/Graphs.pas
@@ -2,21 +2,40 @@
 
 var
   x, y, prevX, prevY: real;
+  deltaX, deltaY:integer;
   i: integer;
   
 begin 
    SetWindowWidth(800);
    SetWindowHeight(600); 
    
-   Line(40, 200, 40, -400); 
-   Line(40, 200, 400, 200); 
+   deltaX := 100;
+   deltaY := 400;
    
-   prevX := 40;
-   prevY := 200; 
+   Line(deltaX, deltaY, deltaX, -deltaY); 
+   Line(deltaX, -deltaY, deltaX, 10*deltaX); 
+   Line(deltaX, deltaY, 10*deltaX, deltaY);
+   
+   for i := 1 to 8 do
+   begin
+     Line(deltaX + 45*i, 5+deltaY, deltaX+45*i, -5+deltaY);
+     SetFontSize(8); 
+     TextOut(deltaX + 45*i - 6, deltaY+10, 45*i);
+   end;
+   
+   for i := -4 to 4 do
+   begin
+     Line(deltaX-5, round(deltaY -100*i/2), deltaX+5, round(deltaY -100*i/2));
+     SetFontSize(8); 
+     TextOut(deltaX-20, round(deltaY-5-100*i/2), i/2);
+   end;
+   
+   prevX := deltaX;
+   prevY := deltaY; 
    for i := 0 to 360 do
    begin
-     x := 40 + i;
-     y := -sin(i * pi / 60) * 100  + 200; 
+     x := deltaX + i;
+     y := -sin(i * pi / 180) * 100  + deltaY; 
      line(round(prevX), round(prevY), round(x), round(y)); 
      prevx := x;
      prevy := y;
